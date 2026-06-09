46b76c0b158aad8f12b883a748a7ed847cd4e9c2
TheDeveloper <171641141+JusticeCodes00@users.noreply.github.com>
2026-06-09 18:51:21 +0100

    feat(server): setup express on port 3000

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import methodOverride from "method-override";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.set("views", join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+
+  console.log(`Server is running on port ${PORT}.`);
+});
